87af158aec5612daa929a11022f5bebfbf659c66
Julian Alfonso Ponce <hello@julianponce.com>
2017-01-27 10:52:28 +0800

    Added default

--- app/controllers/v1/artists_controller.rb
+++ app/controllers/v1/artists_controller.rb
@@ -2,11 +2,17 @@ class V1::ArtistsController < ApplicationController
   def index
     artists = ArtistService::Search.query(params[:query])
     data =
-      artists.map do |artist|
+      artists.map_with_index do |artist, idx|
         data = {}
         %w(id name external_urls genres href).each do |attribute|
           data[attribute] = artist[attribute]
         end
+
+        # NOTE: Default should be the first result? Since it's more popular?
+        data['default'] = idx == 0
+
+        # NOTE: LOL NO LOGIC FOR DEFAULT JUST CHECKED IF IT'S A FAVORITE
+        # data['default'] = Favorite.find_by(spotify_id: artist['id']).any?
         data
       end
     render json: { data: data }
